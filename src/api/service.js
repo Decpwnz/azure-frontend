392854c7a15cbfc93d16da33bf59e1645bc933b5
Decpwnz <35470684+Decpwnz@users.noreply.github.com>
2023-07-10 16:24:23 +0300

    Update service.js

--- src/api/service.js
+++ src/api/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
-const API_ENDPOINT = 'http://ec2-16-171-149-20.eu-north-1.compute.amazonaws.com:3001';
+// const API_ENDPOINT = 'http://ec2-16-171-149-20.eu-north-1.compute.amazonaws.com:3001';
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT || process.env.VITE_API_ENDPOINT;
 
 const api = axios.create({
   baseURL: API_ENDPOINT,
