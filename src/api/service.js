467523fa40983ffe77f37f715dc2052d0c7e04ee
Decpwnz <35470684+Decpwnz@users.noreply.github.com>
2023-07-10 18:14:03 +0300

    Update service.js

--- src/api/service.js
+++ src/api/service.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
-// const API_ENDPOINT = 'http://ec2-16-171-149-20.eu-north-1.compute.amazonaws.com:3001';
-// const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
-const API_ENDPOINT = import.meta.env.VITE_ABOBA_API;
+let API_ENDPOINT;
+
+if (import.meta.env.VITE_API_ENDPOINT !== undefined) {
+  // Vite environment
+  API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+} else if (process.env.VITE_API_ENDPOINT !== undefined) {
+  // Non-Vite environment
+  API_ENDPOINT = process.env.VITE_API_ENDPOINT;
+} else {
+  // Default value if neither variable is defined
+  API_ENDPOINT = 'ec2-16-171-25-86.eu-north-1.compute.amazonaws.com:3001';
+}
+
 
 const api = axios.create({
   baseURL: API_ENDPOINT,
